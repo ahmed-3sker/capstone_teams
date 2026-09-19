27fb9499a6660f82dd4afd3bec3172de1ed34983
ahmedmaz-onlyone <ahmedmostafamaz123@gmail.com>
2026-09-19 03:39:24 +0300

    finish the frist step of creation team the small window appear so i will coninue to add the data and finish the design

---
+++ src/js/home.js
@@ -0,0 +1,59 @@
+let create=document.getElementById('create');
+
+
+let member1_inp= document.getElementById('member1');
+let member2_inp= document.getElementById('member2');
+let member3_inp= document.getElementById('member3');
+let member4_inp= document.getElementById('member4');
+let member5_inp= document.getElementById('member5');
+
+let hidden_popout =document.getElementById('hidden-create');
+
+let contact_inp= document.getElementById('contact');
+
+
+
+
+create.addEventListener( "click" , function() {
+    hidden_popout.style.display='flex';
+    alert('your team has been created.');
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
