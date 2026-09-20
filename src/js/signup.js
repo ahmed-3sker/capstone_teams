b143c1fa2e211d46b29b82f54dca73aad33d4017
Abdelrhman <abdelrazkforman966@gmail.com>
2026-09-21 01:58:22 +0300

    finishing the logic of the sign up page by js and increase the number of the information i will need for the dashboard

---
+++ src/js/signup.js
@@ -0,0 +1,22 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const code = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const repeat = document.getElementById("repeat").value;
+    const numb = document.getElementById("number").value;
+    const name = document.getElementById("name").value;
+
+    if (repeat === password) {
+        localStorage.setItem("ucode", code);
+        localStorage.setItem("upassword", password);
+        localStorage.setItem("unumber", numb); 
+        localStorage.setItem("uname", name);
+        alert("Account created successfully. Now you can log in.");
+    }
+    else{
+        alert("The password and its confirmation are diffrene");
+    }
+})
