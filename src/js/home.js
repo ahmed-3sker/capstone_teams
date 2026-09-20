16757fc675777ae3447ab044057e6a91fc48fd94
ahmedmaz-onlyone <ahmedmostafamaz123@gmail.com>
2026-09-21 00:49:34 +0300

    finish the home page and the leave team function

--- src/js/home.js
+++ src/js/home.js
@@ -13,6 +13,12 @@ let contact_inp= document.getElementById('contact');
 
 let conferm = document.getElementsByClassName('conf')[0];
 
+let leave_form = document.getElementById('hidden-leave');
+let user_leave = document.getElementById('hid-user-leave');
+let code = document.getElementById('hid-code');
+let confirm_leave = document.getElementsByClassName('conf-leave')[0];
+let leave_btn = document.getElementsByClassName('delete')[0];
+
 
 
 create.addEventListener( "click" , function() {
@@ -31,10 +37,61 @@ conferm.addEventListener('click', function(){
     contact_inp.value = phone.value;
     user.value='';
     phone.value='';
+})
 
+leave_btn.addEventListener('click' , function(){
+    leave_form.style.display='flex';
+})
 
-    
+confirm_leave.addEventListener('click', function() {
 
+    if (user_leave.value == member1_inp.value) {
+
+        member1_inp.value = 'none';
+        leave_form.style.display = 'none';
+        alert('You left the team.');
+
+    }
+    else if (user_leave.value == member2_inp.value) {
+
+        member2_inp.value = 'none';
+        leave_form.style.display = 'none';
+        alert('You left the team.');
+
+    }
+    else if (user_leave.value == member3_inp.value) {
+
+        member3_inp.value = 'none';
+        leave_form.style.display = 'none';
+        alert('You left the team.');
+
+    }
+    else if (user_leave.value == member4_inp.value) {
+
+        member4_inp.value = 'none';
+        leave_form.style.display = 'none';
+        alert('You left the team.');
+
+    }
+    else if (user_leave.value == member5_inp.value) {
+
+        member5_inp.value = 'none';
+        leave_form.style.display = 'none';
+        alert('You left the team.');
+
+    }
+    else {
+
+        alert('Check your data again. The name was not found.');
+
+    }
+    // i dont know the logic of connect it but it was the logic of how it work if it connected 
+
+    member5_inp.value=='none';
+    member2_inp.value=='none';
+    member1_inp.value=='none';
+    member3_inp.value=='none';
+    member4_inp.value=='none';
 
 })
 
